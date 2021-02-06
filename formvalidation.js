0fbdff98b213a4a55429103f348422cac7fd861c
Michel Miléski <michel.mileski@gmail.com>
2021-02-06 20:29:47 -0300

    criando div com texto de erro (incluindo tags html) dentro de uma div logo abaixo do campo, com base no data-error-from < atributo criado para tal função

--- formvalidation.js
+++ formvalidation.js
@@ -6,7 +6,15 @@
  * 
  * TODO: criar classe de validação com funções para cada tipo de campo, e incluir validação para radioField
  * TODO: necessita criar funções de validação de CPF, CNPJ entre outros, para validar direto com base nos data-attributes
- * TODO: criar div para reportar erro geral (alerta que existe erros) e informar o erro abaixo de cada campo
+ * TODO: criar div para reportar erro geral (alerta que existe erros)
+ * ! js > local dos itens de insert html
+ * ! <!-- beforebegin -->
+ * !    <div id="mydiv">
+ * !        <!-- afterbegin -->
+ * !            <p>Existing content in #mydiv</p>
+ * !        <!-- beforeend -->
+ * !    </div>
+ * !<!-- afterend -->
  * 
  */
 if(document.querySelectorAll('.formValidationJs')) {
@@ -17,7 +25,7 @@ if(document.querySelectorAll('.formValidationJs')) {
             let submitloginForm = document.querySelector(`#${formToValidate.id} button[type="submit"]`);
             submitloginForm.addEventListener("click", () => {
                 event.preventDefault();
-                console.log(`Formulário: ${formToValidate.id}`);
+                console.log(`================ Formulário: ${formToValidate.id}`);
                 let types = ['input', 'select', 'textarea'];
                 types.forEach((type) => {
                     if(document.querySelectorAll(`#${formToValidate.id} ${type}`)) {
@@ -27,11 +35,20 @@ if(document.querySelectorAll('.formValidationJs')) {
                                 console.log(`Campo: ${field.id} Type: ${field.type} Value: '${field.value}'`);
                                 field.classList.remove("have-success");
                                 field.classList.remove("have-error");
-                                if(field.value == "") {
-                                    field.classList.add("have-error");
-                                    erro = true;
+                                if(document.querySelector(`[data-error-from="${field.id}"]`)) {
+                                    document.querySelector(`[data-error-from="${field.id}"]`).remove();
+                                }
+
+                                if(field.type == 'checkbox') {
+
                                 } else {
-                                    field.classList.add("have-success");
+                                    if(field.value == "") {
+                                        field.classList.add("have-error");
+                                        displayMsg(field.id, field.getAttribute("data-error-message"));
+                                        erro = true;
+                                    } else {
+                                        field.classList.add("have-success");
+                                    }
                                 }
                             }
                         });
@@ -40,4 +57,11 @@ if(document.querySelectorAll('.formValidationJs')) {
             });
         }
     });
+}
+
+function displayMsg(idOrigem, mensagem) {
+    if(!mensagem) {
+        var mensagem = "Este campo nessecita ser preenchido.";
+    }
+    document.querySelector(`#${idOrigem}`).insertAdjacentHTML("afterend", `<div class='this-error' data-error-from='${idOrigem}'>${mensagem}</div>`);
 }
